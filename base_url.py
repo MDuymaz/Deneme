1a0bf2914df78371c7b25899a86a4502c98d1f23
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-03-15 23:29:01 +0300

    Update base_url.py

--- base_url.py
+++ base_url.py
@@ -1,17 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import time
 
-# WebDriver'ı ayarla
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+# Chrome seçeneklerini ayarla
+chrome_options = Options()
 
-# Siteyi ziyaret et
+# Farklı bir kullanıcı veri dizini kullan
+chrome_options.add_argument("--user-data-dir=/tmp/chrome-data")  # Geçici bir dizin oluşturulabilir
+
+# WebDriver'ı başlat
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+# Sayfayı aç
 url = "https://trgoals1235.xyz/"
 driver.get(url)
 
-# Sayfanın tamamen yüklenmesi için bir süre bekle (Gerekirse süreyi arttırabilirsiniz)
+# Sayfanın tamamen yüklenmesi için bir süre bekle
 time.sleep(5)
 
 # Sayfanın HTML içeriğini al
