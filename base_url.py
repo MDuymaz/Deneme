cab524e75f0819a8d548afdd2d14b96cb4063e40
MDuymaz <81718348+MDuymaz@users.noreply.github.com>
2025-03-15 23:42:24 +0300

    Update base_url.py

--- base_url.py
+++ base_url.py
@@ -6,37 +6,29 @@ import time
 
 # Chrome seçeneklerini ayarla
 chrome_options = Options()
-
-# User data directory ile ilgili hatayı engellemek için --no-sandbox ve --disable-dev-shm-usage ekleyelim
+chrome_options.add_argument("--headless")  # Başsız (headless) modda çalıştırmak için
 chrome_options.add_argument("--no-sandbox")  # Sandbox kullanmamak
 chrome_options.add_argument("--disable-dev-shm-usage")  # /dev/shm kullanımını engelle
-
-# Başsız (headless) modda çalıştırmak isterseniz
-chrome_options.add_argument("--headless")  # GUI'siz modda çalıştır
 
 # Tarayıcıyı başlatmak için gerekli path ve driver'ı ayarla
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 # Sayfayı aç
-url = "https://trgoals1235.xyz/"
+url = "https://trgoals1235.xyz//channel.html?id=yayin1"
 driver.get(url)
 
 # Sayfanın tamamen yüklenmesi için bir süre bekle
 time.sleep(5)
 
-# Sayfanın HTML içeriğini al
-html_content = driver.page_source
+# JavaScript ile tanımlanmış olan baseurl değişkenini almak
+baseurl = driver.execute_script("return baseurl;")
 
-# 'baseurl' kelimesini ara
-if "baseurl" in html_content:
-    print("HTML içinde 'baseurl' kelimesi bulundu.")
-    
-    # HTML içeriğini base_url.txt dosyasına yazdır
-    with open("base_url.txt", "w", encoding="utf-8") as file:
-        file.write(html_content)
-    print("HTML içeriği base_url.txt dosyasına yazıldı.")
-else:
-    print("HTML içinde 'baseurl' kelimesi bulunamadı.")
+# baseurl'i yazdır
+print("Base URL:", baseurl)
+
+# baseurl değerini base_url.txt dosyasına yaz
+with open("base_url.txt", "w", encoding="utf-8") as file:
+    file.write(baseurl)
 
 # Tarayıcıyı kapat
 driver.quit()
